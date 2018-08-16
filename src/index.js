11dd1131bb9866d289019da2f2cc61b002efe8ae
Rai Vitor <raivitor@gmail.com>
2018-08-16 20:08:08 -0300

    Added firebase

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase'
 
+var config = {
+	apiKey: "AIzaSyC3AlhX86JBMrbzacSx1Hjj__RfIHehVow",
+	authDomain: "hackathon-c5036.firebaseapp.com",
+	databaseURL: "https://hackathon-c5036.firebaseio.com",
+	projectId: "hackathon-c5036",
+	storageBucket: "hackathon-c5036.appspot.com",
+	messagingSenderId: "631159823326"
+};
+firebase.initializeApp(config);
+  
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
